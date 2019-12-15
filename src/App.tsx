4292b6ac6251685b1aa3813cacf42b00e4e998ec
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-15 20:35:21 +0100

    add react-bootstrap & navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Landing from "./pages/Landing";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Landing />
       <header className="App-header">
         <p>Welcome to the Agro Info App!!!</p>
       </header>
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationMenu from "../components/Navbar";
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <NavigationMenu />
+      <p>Landing Page</p>
+    </div>
+  );
+};
+
+export default Landing;
