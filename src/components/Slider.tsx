802a50c7e89e6e5aaca18e7888f86d2c2e4ce9ed
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-15 20:58:47 +0100

    add carousel to the landing

---
+++ src/components/Slider.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+const Slider: React.FC = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1555952678-9da903a4cd5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1457530378978-8bac673b8062?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1450528039619-bdc0c2d26850?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1492114011589-509c6e695d8e?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>4 slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Slider;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavigationMenu from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <NavigationMenu />
+      <Slider />
       <p>Landing Page</p>
     </div>
   );
