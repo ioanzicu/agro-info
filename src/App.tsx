d8ecc229b3640b6352570853333f47f418a25cbe
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-20 19:18:34 +0100

    add home and dashbord routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 import Landing from "./pages/Landing_page";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Dashboard from "./pages/Dashboard_page";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Route path="/" exact component={Landing} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
     </div>
   );
 };
