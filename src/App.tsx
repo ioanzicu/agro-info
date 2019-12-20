e4516fb3c59425feddd68e1072bb9fb2d072e085
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-20 17:56:18 +0100

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Landing from "./pages/Landing_page";
+import { Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard_page";
+import { Link } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Landing />
-      <Dashboard />
+      <Route path="/" exact component={Landing} />
+      <Route path="/dashboard" component={Dashboard} />
     </div>
   );
 };
